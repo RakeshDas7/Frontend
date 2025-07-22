778c46fe5f7d5f267b7ffcc6aac08cb8c7eb1713
RakeshDas7 <rakeshdas1477@gmail.com>
2025-07-22 07:52:00 +0530

    data types and variable in java script

---
+++ Day 11/script.js
@@ -0,0 +1,97 @@
+typeof 7
+typeof 100
+typeof 7.8
+typeof 13.23
+// typeof "hello"
+// typeof hello
+// 'undefined'
+// typeof "hello"
+// 'string'
+// typeof "h"
+// 'string'
+// typeof'h'
+// 'string'
+// typeof `k`
+// 'string'
+// typeof true
+// 'boolean'
+// typeof ''
+// 'string'
+
+// typeof "I'm Rakesh"
+// 'string'
+// typeof '""'
+// 'string'
+// 'I"m Sai'
+// 'I"m Sai'
+// // 'Hello I am Rakesh
+// // J'
+// // VM1041:1 Uncaught SyntaxError: Invalid or unexpected tokenUnderstand this error
+// `Hello I am Rakesh
+// J`
+// 'Hello I am Rakesh\nJ'
+
+'hii' + 
+    'I am ' + '\t' + 
+    'Rakesh'
+'hiiI am \tRakesh'
+typeof 100
+'number'
+typeof '100'
+'string'
+typeof + '100'
+'number'
+
+parseInt ('100')
+100
++'100'
+100
++'100abc'
+NaN
+typeof nan
+'undefined'
+parseInt('100asd')
+100
+parseInt('10cfgh100asd')
+10
+parseInt('ujk10cfgh100asd')
+NaN
+
+parseFloat('100.12')
+100.12
+parseInt('100.12')
+100
+100+''
+'100'
+100+'' + 20
+'10020'
+
+true
+true
+false
+false
+typeof true
+'boolean'
++ true
+1
++ false
+0
+
++ true + false
+1
+undefined
+undefined
+true
+true
+typeof undefined
+'undefined'
++ undefined
+NaN
+let a = 10
+undefined
+a
+10
+let b;
+undefined
+b
+undefined
