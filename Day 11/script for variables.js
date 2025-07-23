12fb0093c9dba112c45a63ea17b9902bf0c064a4
RakeshDas7 <rakeshdas1477@gmail.com>
2025-07-23 07:54:13 +0530

    Javascript types of variables and defined

---
+++ Day 11/script for variables.js
@@ -0,0 +1,69 @@
+let username = 'Akash'
+console.log(username);
+console.log(typeof username);
+
+let age = 20
+console.log(age);
+console.log(typeof age);
+
+let isHappy = true
+console.log(isHappy);
+console.log(typeof isHappy);
+
+let lastName = 'Singh';
+console.log(lastName);
+console.log(typeof lastName);
+
+username = 'Sipun'
+username = 100
+console.log(username);
+console.log(typeof username);
+
+let userIntro = "Hii I'm " + username + ' '+  lastName + " I'm " + age + " years old";
+console.log(userIntro);
+
+let a = undefined
+console.log(a);
+console.log(typeof a);
+
+let b;
+console.log(b);
+console.log(typeof b);
+
+b = false
+console.log(b);
+console.log(typeof b);
+console.log(b, typeof b, 'xyz');
+
+let c = null
+console.log(c, typeof c);
+
+// <================================================================================================>
+const hoursInADay = 24;
+// hoursInADay = 20
+
+// const d;
+// <=================================================================================================>
+var e
+e = 100;
+console.log(e)
+
+var f 
+console.log(f)
+
+var g = 1120
+{
+    var g = 20
+    // var h = 30 // it will be access outside this block 
+    let h = 30 // it can't be accessible outside this block
+}
+
+console.log(g);
+// console.log(h)
+
+// console.log(x) // cannot access x before initialisation
+// let x = 2025
+
+console.log('Befor initialisation the value of y : '+y); // undefined
+var y = 3020
+console.log(y);
