a1844d081822fcd0d3a8dc4a79c9e11980a4fbe8
RakeshDas7 <rakeshdas1477@gmail.com>
2025-07-27 08:07:05 +0530

    callstack in Javascript

---
+++ Day 15 Hosting (important)/callstack in js.js
@@ -0,0 +1,28 @@
+debugger
+console.log("Execution started")
+function A() {
+    console.log("A")
+}
+A()
+
+function B() {
+    console.log("B")
+}
+B()
+
+function C() {
+    console.log("C1")
+    B();
+    console.log("C2")
+}
+C()
+
+function D() {
+    console.log("D1")
+    C();
+    console.log("D2")
+}
+D()
+
+console.log("Execution ended")
+
