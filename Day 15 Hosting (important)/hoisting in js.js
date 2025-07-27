5dcdb961977ce2c98193e240654ecc61e9f5931e
RakeshDas7 <rakeshdas1477@gmail.com>
2025-07-27 07:36:11 +0530

    hoisting in Javascript

---
+++ Day 15 Hosting (important)/hoisting in js.js
@@ -0,0 +1,11 @@
+console.log(userName)
+var userName = "John";
+console.log(userName)
+
+// console.log(lastName)
+// const lastName = "Daniel" // only variable declared in let and const can be stored in the script scope
+
+sayHii()
+function sayHii() {
+    console.log("Hello Everyone!!")    
+}
