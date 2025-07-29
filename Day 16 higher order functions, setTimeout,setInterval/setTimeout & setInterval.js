df50db67f67cded993c8af86d1b41271ceaefdd3
RakeshDas7 <rakeshdas1477@gmail.com>
2025-07-29 07:46:09 +0530

    setTimeout & setInterval

---
+++ Day 16 higher order functions, setTimeout,setInterval/setTimeout & setInterval.js
@@ -0,0 +1,18 @@
+// console.log(1)
+
+// setTimeout(hello, 2000)
+
+function hello(){
+    console.log("Hello everyone!!!")
+}
+
+// console.log(3)
+
+// // setTimeout(() => {
+// //     console.log("delayed for 5 second")
+// // }, 5000);
+
+// const id = setInterval(hello, 3000)
+// console.log(id)
+
+console.log(setInterval(hello, 2000))
