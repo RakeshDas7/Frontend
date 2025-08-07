555fceaebf3b0f3743f9bf8e9071714468ccaa78
RakeshDas7 <rakeshdas1477@gmail.com>
2025-08-07 06:48:48 +0530

    BOM in Javascript

---
+++ Day 22 BOM/bom.js
@@ -0,0 +1,18 @@
+// console.log(location.reload())
+// location.href = "index.html"
+
+// history represents how many pages are there availble in the browser
+
+// history.go()
+// history.back()
+// history.go(-1) = history.back()
+
+// innerWidth
+// innerHeight
+
+// outerHeight
+// outerWidth
+
+// open("link")
+
+// close() -> // VM190:1 Scripts may close only the windows that were opened by them. all tabs cann't be close by this method 
