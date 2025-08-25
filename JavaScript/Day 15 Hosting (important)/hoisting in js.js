d8177ee464eb20e3c3efd714e20a704261ffe43e
RakeshDas7 <rakeshdas1477@gmail.com>
2025-08-25 20:18:07 +0530

    New Update for React

--- JavaScript/Day 15 Hosting (important)/hoisting in js.js
+++ JavaScript/Day 15 Hosting (important)/hoisting in js.js
@@ -2,8 +2,8 @@ console.log(userName)
 var userName = "John";
 console.log(userName)
 
-// console.log(lastName)
-// const lastName = "Daniel" // only variable declared in let and const can be stored in the script scope
+console.log(lastName)
+const lastName = "Daniel" // only variable declared in let and const can be stored in the script scope
 
 sayHii()
 function sayHii() {
