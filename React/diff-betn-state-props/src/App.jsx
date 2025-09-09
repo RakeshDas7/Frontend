3187a22ab724544a3ba0f49a5e7c7d96af0e40de
RakeshDas7 <rakeshdas1477@gmail.com>
2025-09-09 20:24:53 +0530

    conditional rendering in react

--- React/diff-betn-state-props/src/App.jsx
+++ React/diff-betn-state-props/src/App.jsx
@@ -1,27 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Greeting from './Greeting'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Greeting from "./Greeting";
 
 function App() {
-  console.log("Parent Component")
-  const [name, setName] = useState('Rahul')
+  // console.log("Parent Component")
+
+  const [name, setName] = useState("Rahul");
+  const [showgreeting, setShowGreeting] = useState(true);
+  console.log("showGreeting", showgreeting);
+
   return (
-      <div style={{padding: '20px'}}>
-          <h1>React Props v/s States</h1>
+    <div style={{ padding: "20px" }}>
+      <h1>React Props v/s States</h1>
 
-          {/* <Greeting name={name} /> */}
+      <button onClick={() => setShowGreeting(!showgreeting)}>
+        {showgreeting === true ? "Hide" : "Show"}
+      </button>
 
-          <Greeting name={name}>
-            <p>This is extra elemnt passed as <strong>children</strong></p>
-            We can pass more elements text components etc...
-          </Greeting>
+      {/* <Greeting name={name} /> */}
 
-          <button style={{padding: '20px', margin: '20px'}} onClick={() => { setName((prev) => prev + 1)}}>Click Me</button>
-      </div>
-  )
+      {/* {
+        showgreeting === true ? "Sai" : "pranab"
+      } */}
+
+      {showgreeting && (
+        <Greeting name={name}>
+          <p>
+            This is extra elemnt passed as <strong>children</strong>
+          </p>
+          We can pass more elements text components etc...
+        </Greeting>
+      )}
+
+      <button
+        style={{ padding: "20px", marginTop: "20px" }}
+        onClick={() => {
+          setName((prev) => prev + 1);
+        }}
+      >
+        Click Me
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
