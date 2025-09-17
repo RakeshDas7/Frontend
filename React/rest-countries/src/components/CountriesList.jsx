5c512263aa6bcbd3eeeeed7be3cb9e0df03e5cc0
RakeshDas7 <rakeshdas1477@gmail.com>
2025-09-17 07:43:37 +0530

    Rest-Country new React App developed

---
+++ React/rest-countries/src/components/CountriesList.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import CountryCard from './CountryCard.jsx'
+
+export default function CountriesList({ query }) {
+  const [countriesData, setCountriesData] = useState([])
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all?fields=name,flags,population,region,capital')
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        setCountriesData(data)
+      })
+  }, [])
+
+  const filteredCountries = countriesData.filter((country) =>
+    country.name.common.toLowerCase().includes(query))
+
+  return (
+    <>
+      <div className="countries-container">
+        {
+          filteredCountries.map(
+            (country, idx) =>
+              <CountryCard
+                key={idx}
+                flag={country.flags.svg}
+                name={country.name.common}
+                population={country.population}
+                region={country.region}
+                capital={country.capital[0]}
+              />
+          )
+        }
+      </div>
+    </>
+  )
+}
---
+++ React/rest-countries/src/main.jsx
@@ -0,0 +1,48 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AboutPage from './components/AboutPage.jsx';
+import Header from './components/Header.jsx';
+import Home from './components/Home.jsx';
+import Errorpage from './components/ErrorPage.jsx';
+import CountryDetails from './components/CountryDetails.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: App,
+    errorElement: <Errorpage />, 
+    children : [
+      {
+        path: 'contact',
+        element: <p>Contact Works</p>
+      },
+      {
+        path: 'about',
+        Component: AboutPage,
+        children: [
+          { path: 'abc' , element: <h1>ABCccc</h1>}
+        ] 
+      },
+      {
+        index : true,
+        Component: Home
+      },
+      {
+        path: 'country',
+        Component:CountryDetails
+      }
+    ]
+  },
+])
+
+createRoot(document.getElementById('root')).render(
+  // <StrictMode>
+    // <App />
+  // </StrictMode>,
+
+  <>
+    <RouterProvider router={router} />
+  </>
+)
